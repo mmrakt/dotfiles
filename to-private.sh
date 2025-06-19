5b180edf525f39bbaaaa42137504fffb49cf42d1
mmrakt <mimuraakkun3@gmail.com>
2025-06-19 21:35:01 +0900

    fix

---
+++ to-private.sh
@@ -0,0 +1,1 @@
+find .config/fish/config.fish -type f -exec sed -i '' "s/01056277/akitomimura/g" {} \;
---
+++ to-work.sh
@@ -0,0 +1,1 @@
+find .config/fish/config.fish -type f -exec sed -i '' "s/akitomimura/01056277/g" {} \;
